ec5db9914674f4a417f9cfa25b0e93da5184b603
红血球AE3803 <2544390577@qq.com>
2022-04-11 12:15:05 +0800

    Added QQ Guild support（for  my minecraft server）

--- nonebot_plugin_forwarder/__init__.py
+++ nonebot_plugin_forwarder/__init__.py
@@ -3,9 +3,12 @@ from nonebot.adapters.onebot.v11 import GroupMessageEvent, Bot
 from nonebot.rule import startswith
 from nonebot.typing import T_State
 from nonebot.params import State
+from nonebot.plugin import on_command
+from nonebot.adapters.cqhttp import Bot, MessageSegment
+from nonebot_plugin_guild_patch import GuildMessageEvent
 import asyncio
 
-from .config import forwarder_explict, forwarder_prefix, forwarder_dest_group, forwarder_source_group
+from .config import forwarder_explict, forwarder_prefix, forwarder_source_group, forwarder_source_guild, forwarder_dest_group, forwarder_dest_guild, forwarder_dest_guild_channels, guild_name, guild_link, cids, cnames, group_name
 
 rule = startswith(forwarder_prefix)
 msg_matcher = on_message(rule, priority=10, block=False)
@@ -14,6 +17,8 @@ msg_matcher = on_message(rule, priority=10, block=False)
 async def send_meg(bot: Bot, group_id: str, msg: str):
     await bot.send_group_msg(group_id=group_id, message=msg, auto_escape=False)
 
+async def send_ch(bot: Bot, guild_id: str, channel_id: str, msg: str):
+    await bot.send_guild_channel_msg(guild_id=guild_id, channel_id=channel_id, message=msg, auto_escape=False)
 
 @msg_matcher.handle()
 async def _(bot: Bot, event: GroupMessageEvent, state: T_State = State()):
@@ -24,8 +29,45 @@ async def _(bot: Bot, event: GroupMessageEvent, state: T_State = State()):
             flag = True
         if flag:
             # logger.debug("准备转发")
-            if forwarder_dest_group[0] != "":
+            if forwarder_dest_group[0] == str(event.group_id):
                 msg = event.message
-                # logger.debug(msg)
-                tasks = [send_meg(bot, gid, msg) for gid in forwarder_dest_group]
+                group = str(event.group_id)
+                nickname = event.sender.nickname
+                msgformat = '[' + group_name[0] + ']' + nickname + ' >>> ' + msg + ' (群号: ' + group + ')'
+                tasks = [send_meg(bot, forwarder_dest_group[1], msgformat)]
+                tasks1 = [send_ch(bot, forwarder_dest_guild[0], cid, msgformat) for cid in forwarder_dest_guild_channels]
+                await asyncio.wait(tasks)
+                await asyncio.wait(tasks1)
+            if forwarder_dest_group[1] == str(event.group_id):
+                msg = event.message
+                group = str(event.group_id)
+                nickname = event.sender.nickname
+                msgformat = '[' + group_name[1] + ']' + nickname + ' >>> ' + msg + ' (群号: ' + group + ')'
+                tasks = [send_meg(bot, forwarder_dest_group[0], msgformat)]
+                tasks1 = [send_ch(bot, forwarder_dest_guild[0], cid, msgformat) for cid in forwarder_dest_guild_channels]
+                await asyncio.wait(tasks)
+                await asyncio.wait(tasks1)
+@msg_matcher.handle()
+async def _(bot: Bot, event: GuildMessageEvent, state: T_State = State()):
+    if str(event.guild_id) in forwarder_source_guild:
+        if forwarder_explict:
+            flag = forwarder_explict[0] == "" or str(event.user_id) in forwarder_explict
+        else:
+            flag = True
+        if flag:
+            # logger.debug("准备转发")
+            if forwarder_dest_guild[0] == str(event.guild_id):
+                msg = event.message
+                guild = str(event.guild_id)
+                channel = str(event.channel_id)
+                nickname = event.sender.nickname
+                gname = guild_name[0]
+                glink = guild_link[0]
+                if channel in cids:
+                    cindex = cids.index(channel)
+                    chname = cnames[cindex]
+                    msgformat = '[' + gname +']['+ chname + ']' + nickname + ' >>> ' + msg + ' (加入频道: '+ glink + ')'
+                else:    
+                    msgformat = '[' + gname + ']' + nickname + ' >>> ' + msg + ' (加入频道: '+ glink + ')'
+                tasks = [send_meg(bot, groups, msgformat) for groups in forwarder_dest_group]
                 await asyncio.wait(tasks)
--- nonebot_plugin_forwarder/config.py
+++ nonebot_plugin_forwarder/config.py
@@ -1,26 +1,81 @@
 from nonebot import get_driver, logger
 
 config = get_driver().config
-
+#=====================群聊设置======================
+#源群
 if not hasattr(config, "forwarder_source_group"):
-    forwarder_source_group = [""]
+    forwarder_source_group = ""
     logger.info("未检测到forwarder_source_group配置")
 else:
     forwarder_source_group = config.forwarder_source_group
-
+#目标群
 if not hasattr(config, "forwarder_dest_group"):
     forwarder_dest_group = [""]
     logger.info("未检测到forwarder_dest_group配置")
 else:
     forwarder_dest_group = config.forwarder_dest_group
-
+#群名
+if not hasattr(config, "group_name"):
+    group_name = ["一群","二群"]
+    logger.info("未检测到group_name配置")
+else:
+    group_name = config.group_name
+#===================================================
+#=====================频道设置======================
+#源频道
+if not hasattr(config, "forwarder_source_guild"):
+    forwarder_source_guild = ""
+    logger.info("未检测到forwarder_source_guild配置")
+else:
+    forwarder_source_guild = config.forwarder_source_guild
+#目标频道
+if not hasattr(config, "forwarder_dest_guild"):
+    forwarder_dest_guild = [""]
+    logger.info("未检测到forwarder_dest_guild配置")
+else:
+    forwarder_dest_guild = config.forwarder_dest_guild
+#目标子频道
+if not hasattr(config, "forwarder_dest_guild_channels"):
+    forwarder_dest_guild_channels = [""]
+    logger.info("未检测到forwarder_dest_guild_channels配置")
+else:
+    forwarder_dest_guild_channels = config.forwarder_dest_guild_channels
+#频道名
+if not hasattr(config, "guild_name"):
+    guild_name = ["频道"]
+    logger.info("未检测到guild_name配置")
+else:
+    guild_name = config.guild_name
+#频道链接
+if not hasattr(config, "guild_link"):
+    guild_link = ["请设置链接"]
+    logger.info("未检测到guild_link配置")
+else:
+    guild_link = config.guild_link
+#要关联的子频道ID
+if not hasattr(config, "cids"):
+    cids = [""]
+    logger.info("未检测到cids配置")
+else:
+    cids = config.cids
+#要关联的子频道名
+if not hasattr(config, "cnames"):
+    cnames = [""]
+    logger.info("未检测到cnames配置")
+else:
+    cnames = config.cnames
+#=================================================== 
+#=====================其它设置======================
+#前缀   
 if not hasattr(config, "forwarder_prefix"):
     forwarder_prefix = [""]
-    logger.info("未检测到forwarder_prefix配置，默认为['']")
+    logger.info("未检测到forwarder_prefix配置，默认为 ''")
 else:
     forwarder_prefix = config.forwarder_prefix
+#过滤器
 if not hasattr(config, "forwarder_explict"):
     forwarder_explict = [""]
     logger.info("未检测到forwarder_explict配置，默认为['']")
 else:
     forwarder_explict = config.forwarder_explict
+#=================================================== 
